8175dce1ef0315c8e3ea59be476e5c997e569f39
MrudulaMadhavan <mrudula.madhavan@target.com>
2021-02-27 15:46:49 +0530

    added get endpint for modules'

--- coursemodules/urls.py
+++ coursemodules/urls.py
@@ -1,8 +1,17 @@
 
-from django.urls import path
+from django.urls import include,path
 from .views import ModuleView
+from rest_framework import routers
+from . import views
 
+router = routers.DefaultRouter()
+router.register(r'modules', views.ModuleViewSet)
+
+
+# urlpatterns = [
+#     path('modules', ModuleView.as_view())
+#             ]
 
 urlpatterns = [
-    path('modules', ModuleView.as_view())
-            ]
+    path('', include(router.urls))
+]
