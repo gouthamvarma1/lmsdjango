d937b4450110bc423f21adabd5911ac8a745c8dd
goutham varma <goutham@bluvision.com>
2021-02-28 15:09:42 +0530

    added student models

--- students/models.py
+++ students/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Students(models.Model):
     # course = models.ForeignKey(Course,on_delete=models.CASCADE)
-    id =models.CharField(max_length=50,unique=True)
+    id =models.CharField(max_length=50,primary_key=True)
     name = models.CharField(max_length=50)
     email= models.CharField(max_length=600)
     gender= models.CharField(max_length=6)
